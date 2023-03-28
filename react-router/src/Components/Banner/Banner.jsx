650b1c90180a4e23b069828e28532bd8aadbcf51
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-03-28 11:05:18 -0300

    ..

--- react-router/src/Components/Banner/Banner.jsx
+++ react-router/src/Components/Banner/Banner.jsx
@@ -1,7 +1,28 @@
-import styles from './Banner.module.css';
+import styles from "./Banner.module.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Carousel  from 'react-bootstrap/Carousel';
+import img from "../../assets/carousel.png";
+import img2 from '../../assets/carousel2.png';
+import img3 from '../../assets/carousel3.png';
 
-export default function Banner(){
-    return(
-        <header className={styles.header}></header>
-    )
+
+export default function Banner() {
+
+  return (
+    <header className={styles.header}>
+      <div>
+        <Carousel>
+          <Carousel.Item>
+            <img src={img} alt="" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={img2} alt="img1" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={img3} alt="img1" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </header>
+  );
 }
