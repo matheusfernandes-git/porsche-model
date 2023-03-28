9ce6aa902827559eb9f0f0b1b34dda11228b273e
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-03-28 20:24:13 -0300

    .

--- react-router/src/Components/Menu/Menu.jsx
+++ react-router/src/Components/Menu/Menu.jsx
@@ -1,28 +1,41 @@
-import styles from './Menu.module.css';
-import MenuLink from '../MenuLink/MenuLink';
-import porsche from '../../assets/simbolo.png'
+import styles from "./Menu.module.css";
+import MenuLink from "../MenuLink/MenuLink";
+import porsche from "../../assets/simbolo.png";
+import { FaBars } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
+import { useState } from "react";
 
-export default function Menu(){ 
-    return(
-        <header>
-            <nav className={styles.navegation}>
-                
-                <div className={styles.div__img}>
-                    <img className={styles.img} src={porsche} alt="porsche"/>
-                </div>
+export default function Menu() {
+//   const [sideBar, setSideBar] = useState(false);
 
-               <div className={styles.div__link}>
-                   <MenuLink to='/'>
-                        Home
-                   </MenuLink>
-                   <MenuLink to='/models'>
-                        Models
-                   </MenuLink>
-                   <MenuLink to='/sobre'>
-                        About
-                   </MenuLink>
-               </div>
-            </nav>
-        </header>
-    )
+//   const showSideBar = () => {
+//     setSideBar(!sideBar);
+//   }
+
+//   const closeSideBar = () => {
+//     active(false);
+//   };
+
+  return (
+    <header>
+      <nav className={styles.navegation}>
+        <div className={styles.div__img}>
+          <img className={styles.img} src={porsche} alt="porsche" />
+        </div>
+
+        <div className={styles.div__link}>
+          <MenuLink to="/">Home</MenuLink>
+          <MenuLink to="/models">Models</MenuLink>
+          <MenuLink to="/sobre">About</MenuLink>
+        </div>
+
+        {/* <div>
+          <FaBars onClick={showSideBar} />
+          {sideBar && <SideBar active={setSideBar} />}
+        </div>
+
+        <FaTimes onClick={closeSideBar} /> */}
+      </nav>
+    </header>
+  );
 }
