99947ba0fd0fedf7608855804aa0d3a3aa3842ad
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-03-30 18:32:04 -0300

    .

--- src/Components/PostCard/PostCard.jsx
+++ src/Components/PostCard/PostCard.jsx
@@ -1,20 +1,19 @@
 import { Link } from "react-router-dom";
 import MainButton from "../MainButton/MainButton";
-import styles from "./Post.module.css";
+import styles from "./PostCard.module.css";
 
 export default function PostCard({ post }) {
   return (
     <Link to={`/posts/${post.id}`}>
       <div className={styles.post}>
-        <img
-          src={`/assets/posts/${post.id}/capa.png`}
-          alt="Imagem porsche"
-        >
-        </img>
-        
-        <h2 className={styles.title}>{post.titulo}</h2>
-
-        <MainButton>Ver</MainButton>
+          <img
+            src={`/assets/posts/${post.id}/capa.png`}
+            alt="Imagem porsche"
+          >
+          </img>
+          
+          <h2 className={styles.title}>{post.titulo}</h2>
+          <MainButton>Ver</MainButton>
       </div>
     </Link>
   );
--- src/pagess/Post/Post.jsx
+++ src/pagess/Post/Post.jsx
@@ -1,57 +1,59 @@
-import styles from './Post.module.css';
+import styles from "./Post.module.css";
 
-import { Route, Routes, useParams } from "react-router-dom"
+import { Route, Routes, useParams } from "react-router-dom";
 import TemplatePost from "../../Components/TemplatePost/TemplatePost";
-import posts from '../../json/posts.json'
-import ReactMarkdown from 'react-markdown';
-import './Post.css'
+import posts from "../../json/posts.json";
+import ReactMarkdown from "react-markdown";
+import "./Post.css";
 import Error404 from "../Error404/Error404";
 import DeafultPage from "../../Components/DefaultPage/DefaultPage";
-import PostCard from '../../Components/PostCard/PostCard';
+import PostCard from "../../Components/PostCard/PostCard";
 
-export default function Post(){
-    const params = useParams();
+export default function Post() {
+  const params = useParams();
 
-    const post = posts.find((post) => {
-        return post.id === Number(params.id);
-    })
+  const post = posts.find((post) => {
+    return post.id === Number(params.id);
+  });
 
-    if(!post){
-        return <Error404/>
-    }
+  if (!post) {
+    return <Error404 />;
+  }
 
-     const recommendedPosts = posts.filter((post) => post.id !== Number(params.id))
-     .sort((a, b) => b.id - a.id)  //Ordenando o post em ordem decrescente.
-     .slice(0, 4);        //Pegando somente 4 posts.
+  const recommendedPosts = posts
+    .filter((post) => post.id !== Number(params.id))
+    .sort((a, b) => b.id - a.id) //Ordenando o post em ordem decrescente.
+    .slice(0, 4); //Pegando somente 4 posts.
 
-    return (
-        <Routes>
-            <Route path='*' element={<DeafultPage/>}>
-                <Route index element={
-                    <TemplatePost 
-                        coverPhoto= {`/assets/posts/${post.id}/capa.png`}
-                        title={post.titulo}
-                >
-                    <div className="post-markdown-container">
-                        <ReactMarkdown>
-                           {post.texto}
-                        </ReactMarkdown>   
-                    </div>
+  return (
+    <Routes>
+      <Route path="*" element={<DeafultPage />}>
+        <Route
+          index
+          element={
+            <TemplatePost
+              coverPhoto={`/assets/posts/${post.id}/capa.png`}
+              title={post.titulo}
+            >
+              <div className="post-markdown-container">
+                <ReactMarkdown>{post.texto}</ReactMarkdown>
+              </div>
 
-                    <h2 className={styles.recommendedPostTitle}>
-                        Outros modelos que você pode gostar
-                    </h2>
+              <h2 className={styles.recommendedPostTitle}>
+                Outros modelos que você pode gostar
+              </h2>
 
-                    <ul className={styles.recommendedPost}>
-                        {recommendedPosts.map((post) => (
-                            <li key={post.id}>
-                                <PostCard post={post}/>
-                            </li>
-                        ))}
-                    </ul>
-
-                </TemplatePost>
-                }/>
-            </Route>
-        </Routes>
-)}
+              <ul className={styles.recommendedPost}>
+                {recommendedPosts.map((post) => (
+                  <li key={post.id}>
+                    <PostCard post={post} />
+                  </li>
+                ))}
+              </ul>
+            </TemplatePost>
+          }
+        />
+      </Route>
+    </Routes>
+  );
+}
