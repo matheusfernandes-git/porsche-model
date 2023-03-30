c4d5472e246b06308accb594a8c14c1f126ba8c7
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-03-29 21:01:22 -0300

    ...

--- react-router/src/Components/Menu/Menu.jsx
+++ react-router/src/Components/Menu/Menu.jsx
@@ -1,45 +1,46 @@
-import styles from "./Menu.module.css";
-import MenuLink from "../MenuLink/MenuLink";
-import porsche from "../../assets/simbolo.png";
+import { Links } from "../MenuLink/Links";
+import css from "./Menu.css";
 import Hamburger from 'hamburger-react';
+import porsche from "../../assets/simbolo.png";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import * as AiIcons from "react-icons/ai";
+import * as FaIcons from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 export default function Menu() {
+  const [isOpen, setOpen] = useState(false);
+  const showBurger = () => setOpen(!isOpen);
 
-  // const links = [{
-  //   title: Home,
-  //   to: '/'
-  // },{
-  //   title: Models,
-  //   to: '/models'
-  // },{
-  //   title: About,
-  //   to: '/sobre'
-  // }]
-
-  const [isOpen, setOpen] = useState(false); 
-  const showBurger = () => {
-    console.log('teste')
-    setOpen(!isOpen);
-  }
   return (
     <header>
-      <nav className={styles.navegation}>
-        <div className={styles.div__img}>
-          <img className={styles.img} src={porsche} alt="porsche" />
-        </div>
+      <div className="div_img">
+        <img src={porsche} alt="porsche" />
+      </div>
 
-        <div className={styles.div__link}>
-          <MenuLink to="/">Home</MenuLink>
-          <MenuLink to="/models">Models</MenuLink>
-          <MenuLink to="/sobre">About</MenuLink>
+      <IconContext.Provider value={{}}>
+        <div className="nav_bar">
+          <Link to="#" className="menu-bars">
+            <FaIcons.FaBars onClick={showBurger} />
+          </Link>
         </div>
-
-        <div className={styles.container__burger}>
-          <Hamburger size={25} onToggle={showBurger}>
-          </Hamburger>
-        </div>
-      </nav>
+        <nav className={isOpen ? "nav_menu active" : "nav_menu"}>
+          <ul className="nav_menu_itens" onClick={showBurger}>
+            <li className="navbar_toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {Links.map((link, index) => {
+              return (
+                <li key={index} className={link.cName}>
+                  <Link to={link.to}>{link.title}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
     </header>
   );
 }
---
+++ react-router/src/Components/MenuLink/Links.jsx
@@ -0,0 +1,16 @@
+
+  export const Links = [
+  {
+    title: 'HOME',
+    to: '/',
+    cName: 'nav_text'
+  },{
+    title: 'MODELS',
+    to: '/models',
+    cName: 'nav_text'
+  },{
+    title: 'ABOUT',
+    to: '/sobre',
+    cName: 'nav_text'
+  }
+]
