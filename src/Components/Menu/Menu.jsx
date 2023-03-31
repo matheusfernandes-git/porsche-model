6e25322c472725c2a7c900cd2dc98d7b3b4f9ed9
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-03-31 10:53:36 -0300

    ....

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,11 +1,10 @@
 import { Links } from "../MenuLink/Links";
 import "./Menu.css";
-import porsche from "../../assets/simbolo.png";
+import logo from "../../assets/simbolo2.png";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import * as AiIcons from "react-icons/ai";
 import * as FaIcons from "react-icons/fa";
-import { IconContext } from "react-icons";
 
 export default function Menu() {
   const [isOpen, setOpen] = useState(false);
@@ -14,32 +13,29 @@ export default function Menu() {
   return (
     <header>
       <div className="div_img">
-        <img src={porsche} alt="porsche" />
+        <img src={logo} alt="porsche" />
       </div>
-
-      <IconContext.Provider value={{}}>
-        <div className="nav_bar">
-          <Link to="#" className="menu-bars">
-            <FaIcons.FaBars onClick={showBurger} />
-          </Link>
-        </div>
-        <nav className={isOpen ? "nav_menu active" : "nav_menu"}>
-          <ul className="nav_menu_itens" onClick={showBurger}>
-            <li className="navbar_toggle">
-              <Link to="#" className="menu-bars-closed">
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li>
-            {Links.map((link, index) => {
-              return (
-                <li key={index} className={link.cName}>
-                  <Link to={link.to}>{link.title}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </IconContext.Provider>
+      <div className="nav_bar">
+        <Link to="#" className="menu-bars">
+          <FaIcons.FaBars onClick={showBurger} />
+        </Link>
+      </div>
+      <nav className={isOpen ? "nav_menu active" : "nav_menu"}>
+        <ul className="nav_menu_itens" onClick={showBurger}>
+          <li className="navbar_toggle">
+            <Link to="#" className="menu-bars-closed">
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+          {Links.map((link, index) => {
+            return (
+              <li key={index} className={link.cName}>
+                <Link to={link.to}>{link.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </header>
-  );
+  );
 }
